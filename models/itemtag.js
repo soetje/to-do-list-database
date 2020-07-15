f47e1719c7d47780e59cc6597824a2c77f2c1575
soetje <imredesoet@hotmail.com>
2020-07-15 19:47:10 +0200

    generated tag and item tag

---
+++ models/itemtag.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class itemTag extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  itemTag.init({
+    todoItemId: DataTypes.INTEGER,
+    tagId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'itemTag',
+  });
+  return itemTag;
+};
